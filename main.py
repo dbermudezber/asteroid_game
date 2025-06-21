44bd2606e4c82b3035aa7fc107d5a06ade03d9d8
dileanbermudezber <dileanbermudez@gmail.com>
2025-06-21 19:10:39 -0400

    added triangle shape + player class

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
  pygame.init()
@@ -10,9 +11,11 @@ def main():
  print (f"Screen width: {SCREEN_WIDTH}")
  print (f"Screen height: {SCREEN_HEIGHT}")
  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+ player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
  while True:
   screen.fill("black")
+  player.draw(screen)
   pygame.display.flip()
 
   clock = pygame.time.Clock()
