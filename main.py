8398aa4b950e6473fcb8e3b89b983cba725b34c5
dileanbermudezber <dileanbermudez@gmail.com>
2025-06-20 23:30:48 -0400

    circle shape class

--- main.py
+++ main.py
@@ -15,9 +15,14 @@ def main():
   screen.fill("black")
   pygame.display.flip()
 
+  clock = pygame.time.Clock()
+  dt = 0
+
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
         return
+
+  dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
